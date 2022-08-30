1a7e06b3456d6122eb4a96e846391723fcbbed0f
JimStoneOL <86531826+JimStoneOL@users.noreply.github.com>
2022-08-30 03:39:13 +0300

    zxczxc

--- server.js
+++ server.js
@@ -22,26 +22,3 @@ res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 app.listen(port);
 
-var HtmlWebpackPlugin = require('html-webpack-plugin');
-module.exports = {
-  entry: './app/index.js',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'index_bundle.js',
-    publicPath: '/'
-  },
-  module: {
-    rules: [
-      { test: /\.(js)$/, use: 'babel-loader' },
-      { test: /\.css$/, use: [ 'style-loader', 'css-loader' ]}
-    ]
-  },
-  devServer: {
-    historyApiFallback: true,
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: 'app/index.html'
-    })
-  ]
-};
